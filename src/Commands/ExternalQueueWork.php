f1f30a784940c4e127997532dc46cee6c05f6247
jacek.obst <jacek.obst@emoti.group>
2025-06-12 15:35:56 +0200

    fix: do not instantiate ConsumerInterface implementation right in the command's constructor
    That's because Laravel auto-resolves and boots all console commands registered with $this->commands(...) when you run any Artisan command.

--- src/Commands/ExternalQueueWork.php
+++ src/Commands/ExternalQueueWork.php
@@ -7,19 +7,18 @@ namespace Emoti\CommonResources\Commands;
 use Emoti\CommonResources\Queue\Consumer\ConsumerInterface;
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
 
 class ExternalQueueWork extends Command
 {
     protected $signature = 'common-resources:queue-external:work';
 
-    public function __construct(private readonly ConsumerInterface $consumer)
-    {
-        parent::__construct();
-    }
-
     public function handle(): void
     {
-        $this->consumer->consume(
+        /** @var ConsumerInterface $consumer */
+        $consumer = App::make(ConsumerInterface::class);
+
+        $consumer->consume(
             captureException: fn(Exception $e) => report($e),
         );
     }
