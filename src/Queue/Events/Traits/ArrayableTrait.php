305e6e35287fada907155f67cce2ed5ee3b81e8d
jacek.obst <jacek.obst@emoti.group>
2025-06-13 16:12:01 +0200

    refactor: refactor the fromArray method

--- src/Queue/Events/Traits/ArrayableTrait.php
+++ src/Queue/Events/Traits/ArrayableTrait.php
@@ -7,7 +7,6 @@ namespace Emoti\CommonResources\Queue\Events\Traits;
 use BackedEnum;
 use Ramsey\Uuid\Uuid;
 use ReflectionClass;
-use ReflectionException;
 
 trait ArrayableTrait
 {
@@ -32,7 +31,6 @@ trait ArrayableTrait
 
     /**
      * Creates a new instance of the class from an array of data.
-     * @throws ReflectionException
      */
     public static function fromArray(array $messageContent): static
     {
@@ -41,26 +39,10 @@ trait ArrayableTrait
 
         foreach ($reflection->getProperties() as $property) {
             $name = $property->getName();
-            $value = $messageContent[$name] ?? $messageContent['data'][$name] ?? null;
+            $value = self::getPropertyValueFromArray($messageContent, $name);
             $type = $property->getType();
-
-            if ($value !== null && $type && !$type->isBuiltin()) {
-                $typeName = $type->getName();
-                if (enum_exists($typeName)) {
-                    /** @var BackedEnum $typeName */
-                    $value = $typeName::from($value);
-                } elseif (class_exists($typeName) && method_exists($typeName, 'fromArray')) {
-                    $value = $typeName::fromArray($value);
-                } elseif (class_exists($typeName) && method_exists($typeName, 'from')) {
-                    $value = $typeName::from($value);
-                } elseif (str_contains($typeName, 'UuidInterface')) {
-                    $value = Uuid::fromString($value);
-                }
-            }
-
-            if ($value !== null) {
-                $property->setValue($instance, $value);
-            }
+            $value = self::resolvePropertyValue($value, $type);
+            self::setPropertyValue($property, $instance, $value);
         }
 
         return $instance;
@@ -77,5 +59,36 @@ trait ArrayableTrait
             'eventId' => $this->eventId()->toString(),
             'routingKey' => $this->routingKey(),
         ];
+    }
+
+    private static function getPropertyValueFromArray(array $messageContent, string $name)
+    {
+        return $messageContent[$name] ?? $messageContent['data'][$name] ?? null;
+    }
+
+    private static function resolvePropertyValue($value, $type)
+    {
+        if ($value !== null && $type && !$type->isBuiltin()) {
+            $typeName = $type->getName();
+            if (enum_exists($typeName)) {
+                /** @var BackedEnum $typeName */
+                return $typeName::from($value);
+            } elseif (class_exists($typeName) && method_exists($typeName, 'fromArray')) {
+                return $typeName::fromArray($value);
+            } elseif (class_exists($typeName) && method_exists($typeName, 'from')) {
+                return $typeName::from($value);
+            } elseif (str_contains($typeName, 'UuidInterface')) {
+                return Uuid::fromString($value);
+            }
+        }
+
+        return $value;
+    }
+
+    private static function setPropertyValue($property, $instance, $value): void
+    {
+        if ($value !== null) {
+            $property->setValue($instance, $value);
+        }
     }
 }
