a72521f67bdaf6f10385b1ca6f0eb7ec42f4cdfd
Mikołaj Salamak <mikolaj.salamak@emoti.group>
2025-07-10 12:17:17 +0200

    feat: add Lang enum for language codes

---
+++ src/Enums/Lang.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Emoti\CommonResources\Enums;
+
+enum Lang: string
+{
+    case EE = 'ee';
+    case FI = 'fi';
+    case LT = 'lt';
+    case LV = 'lv';
+    case PL = 'pl';
+}
