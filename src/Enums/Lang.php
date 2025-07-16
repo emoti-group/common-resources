967cb8d4fb8c6c448aab0ee837a835355487cb85
Mikołaj Salamak <mikolaj.salamak@emoti.group>
2025-07-16 10:05:31 +0200

    feat: add fromSite method to Lang enum for site-specific language retrieval

--- src/Enums/Lang.php
+++ src/Enums/Lang.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Emoti\CommonResources\Enums;
 
+use InvalidArgumentException;
+
 enum Lang: string
 {
     case ET = 'et';
@@ -12,4 +14,33 @@ enum Lang: string
     case LV = 'lv';
     case PL = 'pl';
     case RU = 'ru';
+
+    /**
+     * Returns an array of languages based on the provided site.
+     *
+     * @param Site $site The site for which to get the languages.
+     * @return array<int, Lang> An array of Lang enums corresponding to the site.
+     */
+    public static function fromSite(Site $site): array
+    {
+        return match ($site) {
+            Site::PL => [
+                self::PL
+            ],
+            Site::EE => [
+                self::ET,
+                self::RU,
+            ],
+            Site::LT => [
+                self::LT
+            ],
+            Site::LV => [
+                self::LV,
+                self::RU,
+            ],
+            Site::FI => [
+                self::FI
+            ],
+        };
+    }
 }
