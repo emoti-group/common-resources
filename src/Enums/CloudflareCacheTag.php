5c886dc285456b4b932c4f94e811eedd496806db
jacek.obst <jacek.obst@emoti.group>
2025-10-24 10:02:01 +0200

    feat: shorten the cloudflare tags

--- src/Enums/CloudflareCacheTag.php
+++ src/Enums/CloudflareCacheTag.php
@@ -4,12 +4,27 @@ declare(strict_types=1);
 
 namespace Emoti\CommonResources\Enums;
 
+use InvalidArgumentException;
+
 enum CloudflareCacheTag: string
 {
     case SITE = 'site';
-    case PRODUCT_ID = 'product_id';
-    case GENERAL_CATEGORY_ID = 'general_category_id';
-    case MARKETING_CATEGORY_ID = 'marketing_category_id';
-    case LOCATION_ID = 'location_id';
-    case SUPPLIER_ID = 'supplier_id';
+    case PRODUCT_ID = 'pro';
+    case GENERAL_CATEGORY_ID = 'gcat';
+    case MARKETING_CATEGORY_ID = 'mcat';
+    case LOCATION_ID = 'loc';
+    case SUPPLIER_ID = 'sup';
+
+    public function build(Site $site, string|int|null $value = null): string
+    {
+        if ($this === self::SITE) {
+            return sprintf('%s:%s', $this->value, $site->value);
+        }
+
+        if ($value === null) {
+            throw new InvalidArgumentException('Value cannot be null for this tag type.');
+        }
+
+        return sprintf('%s_%s:%s', $site->value, $this->value, $value);
+    }
 }
