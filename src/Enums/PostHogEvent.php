fbb657d9821d7c576393a32f054055c523919048
Mikołaj Salamak <mikolaj.salamak@emoti.group>
2026-01-12 07:06:59 +0000

    enhance PostHogAbstractService with distinctId management and identification logic

--- src/Enums/PostHogEvent.php
+++ src/Enums/PostHogEvent.php
@@ -8,4 +8,9 @@ enum PostHogEvent: string
 {
     case EXCEPTION = '$exception';
     case PAGEVIEW = '$pageview';
+    case CLIENT_LOGIN = 'client_login';
+    case CMS_LOGIN = 'cms_login';
+    case OFFLINESALES_LOGIN = 'offlinesales_login';
+    case LOGOUT = 'logout';
+    case CLIENT_LOGOUT = 'client_logout';
 }
